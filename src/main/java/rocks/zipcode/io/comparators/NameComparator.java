80718273cdd53db7e6ee6c868b62a595e65f2064
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-02-28 14:10:13 -0500

    Finished Item Sorter

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,14 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    public int compare(Item item, Item item2) {
+       return item.getName().compareTo(item2.getName());
+    }
 }
